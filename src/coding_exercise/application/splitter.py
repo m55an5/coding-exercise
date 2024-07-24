c79d5194e04dcb4cf00de910d100d3a979437205
Manjot <m55an5.d3v@gmail.com>
2024-07-24 22:48:36 +1000

    testing and implemmentation complete

--- src/coding_exercise/application/splitter.py
+++ src/coding_exercise/application/splitter.py
@@ -1,14 +1,50 @@
 from coding_exercise.domain.model.cable import Cable
 
-
 class Splitter:
+    MIN_CABLE_LENGTH=2
+    MAX_CABLE_LENGTH=1024
+    MIN_TIMES_VALUE=1
+    MAX_TIMES_VALUE=64
 
-    def __validate(self):
-        valid = True
-        if not valid:
-            raise ValueError
+    def __validate(self, cable_length, times):
+        if not (self.MIN_CABLE_LENGTH <= cable_length <= self.MAX_CABLE_LENGTH):
+            raise ValueError(f"Cable length must be between {self.MIN_CABLE_LENGTH} and {self.MAX_CABLE_LENGTH}")
+        if not (self.MIN_TIMES_VALUE <= times <= self.MAX_TIMES_VALUE):
+            raise ValueError(f"Times value must be between {self.MIN_TIMES_VALUE} and {self.MAX_TIMES_VALUE}")
 
     def split(self, cable: Cable, times: int) -> list[Cable]:
-        self.__validate()
+        self.__validate(cable.length, times)
 
-        return []
+        sub_cables = []
+
+        # find the maximum lenth that can be achieved w.r.t. n (times)
+        max_length = cable.length // (times + 1)
+
+        if max_length < 1:
+            raise ValueError
+
+        # total cables possible with max length (quotient)
+        max_sub_cables = cable.length // max_length
+        
+        for n in range(max_sub_cables):
+            c = Cable(max_length, self.format_cable_name(cable.name, sub_cables))
+            sub_cables.append(c)
+        
+        # find quotient which gives remaing length of original cable
+        remaining_cable_length = cable.length % max_length
+
+        while remaining_cable_length >= max_length:
+            c = Cable(max_length, self.format_cable_name(cable.name, sub_cables))
+            remaining_cable_length -= max_length
+            sub_cables.append(c)
+
+        # if there is still some length of cable left or the reaiming cable is less than
+        # max length calculated earlier 
+        if remaining_cable_length > 0:
+            c = Cable(remaining_cable_length, self.format_cable_name(cable.name, sub_cables))
+            sub_cables.append(c)
+
+        return sub_cables
+
+    def format_cable_name(self, name, list):
+        return f"{name}-{len(list):02}"
--- tests/coding_exercise/application/test_splitter.py
+++ tests/coding_exercise/application/test_splitter.py
@@ -1,8 +1,86 @@
 from assertpy import assert_that
-
+import pytest
 from coding_exercise.application.splitter import Splitter
 from coding_exercise.domain.model.cable import Cable
 
 
 def test_should_not_return_none_when_splitting_cable():
     assert_that(Splitter().split(Cable(10, "coconuts"), 1)).is_not_none()
+
+
+@pytest.mark.parametrize("length, name, times", [
+    (1024, "apples", 64),
+    (2, "apples", 1),
+])
+def test_should_not_raise_value_error_when_valid_cable_length_valid_times(length, name, times):
+    c = Splitter().split(Cable(length, name), times)
+    assert len(c) > 0
+
+
+@pytest.mark.parametrize("length, name, times", [
+    (1024, "bananas", 65),
+    (2, "bananas", 0),
+])
+def test_should_raise_value_error_when_valid_cable_length_invalid_times(length, name, times):
+    splitter = Splitter()
+    with pytest.raises(ValueError):
+        splitter.split(Cable(length, name), times)
+
+
+@pytest.mark.parametrize("length, name, times", [
+    (1025, "oranges", 64),
+    (1, "oranges", 1),
+])
+def test_should_raise_value_error_when_invalid_cable_length_valid_times(length, name, times):
+    splitter = Splitter()
+    with pytest.raises(ValueError):
+        splitter.split(Cable(length, name), times)
+
+
+def test_should_return_formatted_cable_name():
+    for i in range(9,12):
+        cables = [0] * i
+        r = Splitter().format_cable_name("coconuts", cables)
+        assert f"coconuts-{i:02}" == r
+
+
+def test_should_raise_value_error_for_invalid_length_less_than_one():
+    splitter = Splitter()
+    with pytest.raises(ValueError):
+        Splitter().split(Cable(5, "coconuts"), 6)
+
+
+def test_should_not_raise_error_for_valid_remaining_value_of_zero():
+    res = Splitter().split(Cable(5, "coconuts"), 2)
+    assert len(res) == 5
+
+
+def test_should_not_raise_error_for_valid_remaining_value_of_not_zero():
+    res = Splitter().split(Cable(11, "coconuts"), 3)
+    assert len(res) == 6
+  
+
+@pytest.mark.parametrize("length, name, times, result", [
+    (
+        10, "oranges", 1,   [
+                                Cable(5, "oranges-01"),
+                                Cable(5, "oranges-02")
+                            ]
+    ), 
+    (
+        5, "oranges", 2,   [
+                                Cable(1, "oranges-01"),
+                                Cable(1, "oranges-02"),
+                                Cable(1, "oranges-03"),
+                                Cable(1, "oranges-04"),
+                                Cable(1, "oranges-05"),
+                            ]
+    )
+
+])
+def test_should_pass_with_valid_values(length, name, times, result):
+    r = Splitter().split(Cable(length, name), times)
+    assert len(r) == len(result)
+    for i in range(len(r)):
+        assert Cable(r[i].length, r[i].name == result[i])
+    
